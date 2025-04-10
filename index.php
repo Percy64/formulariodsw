29c6c9b4adaa1d1519e37ef3303718c32e610200
Percy64 <104700336+Percy64@users.noreply.github.com>
2025-04-10 13:56:04 -0300

    clase1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <main class="row">
+        <section class = "col_12 flex flex-j-c">
+            <form action="" method="post" class= "col_3 flex flex-flox-c flex-a-c form_01">
+                <h2>Formulario</h2>
+                <input type="text" class="col_10">
+                <input type="text" class="col_10">
+                <button type="button" class="col_5">enviar</button>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
