f7a9ed7d1efe3d2d92931adfebfb4ca15f87ccfc
Percy64 <104700336+Percy64@users.noreply.github.com>
2025-04-11 05:42:05 -0300

    clase10-04

--- index.php
+++ index.php
@@ -9,12 +9,27 @@
 </head>
 <body>
     <main class="row">
+        
+        <header class="col_12 ">
+            <DIV class="col_4">
+                <h1>Hola mundo</h1>
+            </DIV>
+            <nav class="col_8 flex ">
+                <ul class="col_12 flex flex-j-b">
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Servicios</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </nav>
+        </header>
         <section class = "col_12 flex flex-j-c">
             <form action="" method="post" class= "col_3 flex flex-flox-c flex-a-c form_01">
                 <h2>Formulario</h2>
-                <input type="text" class="col_10">
-                <input type="text" class="col_10">
+                <input type="text" class="col_10" placeholder="Nombre" required>
+                <input type="password" class="col_10" placeholder="Contraseña" required>
                 <button type="button" class="col_5">enviar</button>
+                
             </form>
         </section>
     </main>
