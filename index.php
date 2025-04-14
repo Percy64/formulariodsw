489ac197e3711ccaed406f3c81bf6ea431230690
Percy64 <104700336+Percy64@users.noreply.github.com>
2025-04-14 20:38:58 -0300

    clase14-04

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/grid.css">
     <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
@@ -10,19 +11,21 @@
 <body>
     <main class="row">
         
-        <header class="col_12 ">
+        <header class="col_12 header flex flex-a-c">
             <DIV class="col_4">
-                <h1>Hola mundo</h1>
+                <h3>Hola mundo</h3>
             </DIV>
             <nav class="col_8 flex ">
-                <ul class="col_12 flex flex-j-b">
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Nosotros</a></li>
-                    <li><a href="#">Servicios</a></li>
-                    <li><a href="#">Contacto</a></li>
+                <ul class="col_12 flex flex-j-b menu">
+                    <li class="flex "><a href="#" class="sin-subrayado">Inicio</a></li>
+                    <li class="flex"><a href="#" class="sin-subrayado">registro</a></li>
+                    <li class="flex"><a href="#" class="sin-subrayado">Servicios</a></li>
+                    <li class="flex"><a href="#">Contacto</a></li>
+                    <li class="flex"><a href="#">Blog</a></li>
                 </ul>
             </nav>
         </header>
+        
         <section class = "col_12 flex flex-j-c">
             <form action="" method="post" class= "col_3 flex flex-flox-c flex-a-c form_01">
                 <h2>Formulario</h2>
@@ -32,6 +35,37 @@
                 
             </form>
         </section>
+    <footer class="col_12 flex flex-j-c footer">
+        <div class="col_4 flex flex-j-c flex-flox-c flex-a-c">
+            <p>redes  &copy; 2025</p>
+            <ul class="col_12 flex flex-j-c  ">
+                <li>
+                    <a href="https://es-la.facebook.com/">
+                        <img src="img/facebook.png" alt="facebook" class="col_2 flex redes">
+                    </a>
+                </li>
+                <li>
+                <a href="https://www.instagram.com/">
+                    <img src="img/instagram.png" alt="instagram" class="col_2 flex redes">
+                </a>
+                </li>
+                <li>
+                    <a href="https://www.whatsapp.com/">
+                        <img src="img/whatsapp.png" alt="twitter" class="col_2 flex redes">
+                    </a>
+                </li>
+                
+                
+            </ul>
+        </div>
+        <div class="col_4 flex flex-j-c footer">
+            <p>Desarrollado por emanuel</p>
+        </div>
+        <div class="col_4 flex flex-j-c footer">
+            <p>Contacto: emanuelmerlo15@gmail.com</p>
+    </footer>
+
+
     </main>
 </body>
 </html>
